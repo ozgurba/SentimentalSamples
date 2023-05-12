6a12e91bf250235d2076c24f0266f0fa05324b6b
Katatubies <106171864+Katatubies@users.noreply.github.com>
2023-05-12 05:21:21 +0300

    initialcommit

---
+++ SampleSentimentalAnalysisWithJava/StanfordNLPSample/src/main/java/StanfordNLPSample/Application.java
@@ -0,0 +1,11 @@
+package StanfordNLPSample;
+
+public class Application {
+
+    public static void main(String[] args) {
+    	 String text = "This is an excellent book. I enjoy reading it. I can read on Sundays. Today is only Tuesday. Can't wait for next Sunday. The working week is unbearably long. It's awful.";
+
+    	  nlpPipeline.init();
+    	  nlpPipeline.estimatingSentiment(text);    
+    }
+}
